27a52d24c31cee46892793cda814890aa021dd8f
baraian <16649839+RRasda@users.noreply.github.com>
2022-02-10 22:38:21 +0200

    Cart is now on database

--- client/src/components/products/Product.js
+++ client/src/components/products/Product.js
@@ -1,28 +1,38 @@
 import { Link } from "react-router-dom";
 import styles from "./Product.module.css";
 import { Card, Button } from "react-bootstrap";
+import Axios from "axios";
 const Product = (props) => {
-  const addtocart = () => {
-    let cart = JSON.parse(localStorage.getItem("cart"));
+  const addtocart = async () => {
+    // const getCart = await Axios.get("http://localhost:3001/getCart");
+    // let cart = getCart.data;
+    // console.log(cart);
+    // let cart = JSON.parse(localStorage.getItem("cart"));
     const { id } = props.product;
     const product = {
       id: id,
       quantity: 1,
     };
-    if (cart) {
-      let ok = 0;
-      for (let i = 0; i < cart.length && ok === 0; i++) {
-        if (cart[i].id === product.id) {
-          cart[i].quantity = parseInt(cart[i].quantity) + 1;
-          ok = 1;
-        }
-      }
-      if (ok === 0) cart.push(product);
-    } else {
-      cart = [product];
-    }
-    console.log(cart);
-    localStorage.setItem("cart", JSON.stringify(cart));
+    const insertCart = await Axios.post(
+      "http://localhost:3001/insertCart",
+      { product },
+      { withCredentials: true }
+    );
+    console.log(insertCart);
+
+    // if (cart) {
+    //   let ok = 0;
+    //   for (let i = 0; i < cart.length && ok === 0; i++) {
+    //     if (cart[i].id === product.id) {
+    //       cart[i].quantity = parseInt(cart[i].quantity) + 1;
+    //       ok = 1;
+    //     }
+    //   }
+    //   if (ok === 0) cart.push(product);
+    // } else {
+    //   cart = [product];
+    // }
+    // localStorage.setItem("cart", JSON.stringify(cart));
   };
   return (
     <Card key={props.product.id} className={styles.product}>
--- server/index.js
+++ server/index.js
@@ -12,23 +12,21 @@ dotenv.config();
 app.use(express.json());
 app.use(cookieParser());
 app.use(cors({ credentials: true, origin: "http://localhost:3000" }));
-function authorization (req,res,next) {
+function authorization(req, res, next) {
   const token = req.cookies.token;
-  if(!token) {
-      return res.sendStatus(204)
-
+  if (!token) {
+    return res.sendStatus(204);
   }
   try {
-      const data = jwt.verify(token, process.env.TOKEN_SECRET)
-      req.userId = data.id;
-      req.username = data.username;
-      req.role = data.role;
-      return next();
+    const data = jwt.verify(token, process.env.TOKEN_SECRET);
+    req.userId = data.id;
+    req.username = data.username;
+    req.role = data.role;
+    return next();
   } catch {
-      return res.sendStatus(403);
-     
+    return res.sendStatus(403);
   }
-};
+}
 
 app.post("/register", async (req, res) => {
   try {
@@ -82,7 +80,7 @@ app.post("/login", (req, res) => {
                 console.log("login sucessful");
                 const token = generateAccessToken(user);
                 res.cookie("token", token, { maxage: 86400, httpOnly: true });
-                res.status(200).json({message: "Succesfully logged in !"});
+                res.status(200).json({ message: "Succesfully logged in !" });
               } else {
                 console.log("Passwords do not match");
               }
@@ -103,43 +101,114 @@ app.get("/logout", authorization, (req, res) => {
 });
 app.get("/me", authorization, (req, res) => {
   try {
-   
     return res.json({
-      user : {
+      user: {
         id: req.userId,
         username: req.username,
-        role: req.role
-         }
+        role: req.role,
+      },
     });
   } catch (err) {
     res.status(500).send("Unexpected error");
   }
 });
-app.get("/getProducts",(req,res) => {
+app.get("/getProducts", (req, res) => {
+  try {
+    pool.query("SELECT * FROM PRODUCTS", (err, result) => {
+      if (err) {
+        res.status(400);
+      } else {
+        res.status(200).json(result.rows);
+      }
+    });
+  } catch (err) {
+    res.status(500).send("Unexpected error");
+  }
+});
+app.get("/getProduct/id=:id", (req, res) => {
+  const id = req.params.id;
+  try {
+    pool.query("SELECT * FROM PRODUCTS WHERE id=$1", [id], (err, result) => {
+      if (err) {
+        res.status(400);
+      } else res.status(200).json(result.rows[0]);
+    });
+  } catch (err) {
+    res.status(500).send("Unexpected error");
+  }
+});
+app.get("/getCart", authorization, (req, res) => {
   try {
     pool.query(
-      "SELECT * FROM PRODUCTS", (err,result) => {
+      "SELECT * FROM CART WHERE id_user=$1",
+      [req.userId],
+      (err, result) => {
         if (err) {
           res.status(400);
-        } else { 
-          res.status(200).json(result.rows);
-        }
-
+        } else res.status(200).json(result.rows);
       }
     );
   } catch (err) {
     res.status(500).send("Unexpected error");
   }
 });
-app.get("/getProduct/id=:id", (req,res) => {
-  const id =req.params.id;
+app.post("/insertCart", authorization, async (req, res) => {
   try {
-    pool.query("SELECT * FROM PRODUCTS WHERE id=$1",[id], (err,result) => {
-      if(err) {
-        res.status(400);
-      } else
-      res.status(200).json(result.rows[0]);
-    })
+    pool.query(
+      "SELECT * FROM CART WHERE id_user=$1",
+      [req.userId],
+      (err, result) => {
+        if (err) {
+          console.log(err);
+        } else {
+          const { rowCount } = result;
+          if (rowCount === 0) {
+            const product = JSON.stringify(new Array(req.body.product));
+            console.log(product);
+            pool.query(
+              "INSERT INTO CART (id_user,products) VALUES ($1,$2)",
+              [req.userId, product],
+              (err, result) => {
+                if (err) {
+                  res.status(400);
+                } else {
+                  res.status(200).send("Inserted into cart");
+                }
+              }
+            );
+          } else {
+            let cart = result.rows[0].products;
+            let ok = 0;
+            cart.map((product, index) => {
+              if (product.id === req.body.product.id) {
+                product.quantity += req.body.product.quantity;
+                ok = 1;
+                if (product.quantity < 1) {
+                  cart.splice(index, 1);
+                }
+              }
+            });
+            if (ok === 0) {
+              cart.push(req.body.product);
+            }
+            const final = JSON.stringify(cart);
+            pool.query(
+              "UPDATE CART SET products=$1 WHERE id_user=$2",
+              [final, req.userId],
+              (err, result) => {
+                if (err) {
+                  console.log(err);
+                  res.status(400);
+                } else {
+                  console.log("got here2 ");
+                  res.status(200).send("Updated cart");
+                }
+              }
+            );
+          }
+        }
+      }
+    );
   } catch (err) {
     res.status(500).send("Unexpected error");
   }
