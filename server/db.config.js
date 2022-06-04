2fa3efce1f680a6b51045f80164574bc43bfdfe5
andreibaraian1 <baraian.andrei@gmail.com>
2022-06-04 20:52:44 +0300

    dbconfig

--- server/db.config.js
+++ server/db.config.js
@@ -1,20 +1,15 @@
 const { Pool } = require("pg");
 require("dotenv").config();
-const connectionString = process.env.DATABASE_URL || null;
-let pool;
-if (connectionString) {
-  pool = new Pool({ connectionString });
-} else {
-  pool = new Pool({
-    user: process.env.USER,
-    password: process.env.PASSWORD,
-    host: process.env.HOST,
-    port: process.env.DATABASE_PORT,
-    database: process.env.DATABASE,
-    ssl: {
-      rejectUnauthorized: false,
-    },
-  });
-}
+
+const pool = new Pool({
+  user: process.env.USER,
+  password: process.env.PASSWORD,
+  host: process.env.HOST,
+  port: process.env.DATABASE_PORT,
+  database: process.env.DATABASE,
+  ssl: {
+    rejectUnauthorized: false,
+  },
+});
 
 module.exports = pool;
